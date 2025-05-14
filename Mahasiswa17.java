594651afda7b7932ff8fb2fe55ccf56dc7201215
LAPTOP-RBLOQQKD\Naswa Nida <naswanida3@gmail.com>
2025-05-14 15:32:10 +0700

    Percobaan 1

---
+++ Mahasiswa17.java
@@ -0,0 +1,14 @@
+public class Mahasiswa17 {
+    String nim, nama,kelas; 
+    double ipk;
+    
+    Mahasiswa17(String nim, String nama, String kelas, double ipk) {
+        this.nim = nim;
+        this.nama = nama;
+        this.kelas = kelas;
+        this.ipk = ipk;
+    }
+    public void tampilInformasi() {
+        System.out.println(nama + " \t " + nim + " \t " + kelas + " \t " + ipk);
+    }
+}
